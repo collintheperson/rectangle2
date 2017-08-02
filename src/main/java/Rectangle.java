764f747e6933de4399349d05635ed8a67cc5734c
Guest User <Guest@Epicodus-6E.local>
2017-08-02 09:52:16 -0700

    stuff

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Guest on 8/2/17.
+ */
+public class Rectangle {
+    private int mLength;
+    private int mWidth;
+
+    public Rectangle(int length, int width)  {
+        mLength=length;
+        mWidth=width;
+    }
+
+    public int getLength()  {
+        return mLength;
+    }
+    public int getWidth()  {
+        return mWidth;
+    }
+    public boolean isSquare()   {
+        return mLength == mWidth;
+    }
+
+
+}
